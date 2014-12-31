95611935d3a4b971ea48931fd4894b987118ba30
Erica Johnson <edj@uoregon.edu>
2014-12-30 20:54:41 -0800

    added lifespan attribute for cell objects determined by allele attributes

--- cell.py
+++ cell.py
@@ -9,6 +9,7 @@ class Cell:
 	    self.allele_1 = allele_1
 	    self.allele_2 = allele_2
 	    self.BMR = self.find_BMR()
+	    self.lifespan = self.find_lifespan()
 	    
 	def dominant_allele(self):
 	    if self.allele_2 in self.allele_1['dominant_over']:
@@ -23,5 +24,13 @@ class Cell:
 	    if dominant == 'neither':
 	        average_BMR = (self.allele_1['BMR'] + self.allele_2['BMR'])/2
 	        return average_BMR
+	    else:
+	        return dominant['BMR']
+	        
+	def find_lifespan(self):
+	    dominant = self.dominant_allele()
+	    if dominant == 'neither':
+	        average_lifespan = (self.allele_1['lifespan'] + self.allele_2['lifespan'])/2
+	        return average_lifespan
 	    else:
 	        return dominant['BMR']
