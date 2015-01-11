7bb8cd0faad753906e0dad288f6865d734a93f7e
Erica Johnson <edj@uoregon.edu>
2015-01-10 21:12:08 -0800

    renders the layout.html template

--- cell_evolution.py
+++ cell_evolution.py
@@ -7,13 +7,13 @@
     7 Jan 2015
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'CELL EVOLUTION'
+    return render_template('layout.html')
     
 if __name__ == '__main__':
     app.run(debug = True)
