fd042bedb529bb117600d485b2161852e921936e
Erica Johnson <edj@uoregon.edu>
2015-01-17 22:38:10 -0800

    cell_evolution.py now writes color data to be used on cells to a JSON file

--- cell_evolution.py
+++ cell_evolution.py
@@ -8,6 +8,32 @@
 """
 
 from flask import Flask, render_template
+import json
+from cell import Cell
+import random
+
+cell_list = []
+
+alleleA = {'dominant_over': [], 'BMR': 20, 'lifespan': 20}
+alleleB = {'dominant_over': [alleleA], 'BMR': 30, 'lifespan': 30}
+alleleC = {'dominant_over': [alleleB], 'BMR': 10, 'lifespan': 10}
+alleleD = {'dominant_over': [alleleC], 'BMR': 15, 'lifespan': 15}
+
+cell1 = Cell(alleleA, alleleB)
+cell2 = Cell(alleleC, alleleB)
+cell3 = Cell(alleleD, alleleD)
+
+cells = [cell1, cell2, cell3]
+
+for cell in cells:
+    color = "%06x" % random.randint(0,0xFFFFFF)
+    for i in range(random.randint(1, 5)):
+        cell_list.append({
+            'color' : color
+            })
+
+out_file = open("cells.json", "w")
+json.dump(cell_list, out_file, indent=4)
 
 app = Flask(__name__)
 
